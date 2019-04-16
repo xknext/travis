d052da42055927a82c73f8733ac7ba088f07596a
Sidorovich Nikolay <sidorovichnv@sibedge.com>
2019-04-16 10:27:12 +0700

    app.js correct

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
-});
+}).listen(1337, '127.0.0.1');;
+console.log('Server running at http://127.0.0.1:1337/');
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//server.listen(port, hostname, () => {
+//  console.log(`Server running at http://${hostname}:${port}/`);
+//});
